c6875ee28fea475d90dc6b1bd51eae8ec5014d01
FuLeiCai <18871566937@163.com>
2026-08-21 21:22:45 +0800

    feat: 新增 ODB 数据库封装（mysql_settings 配置与 ODBFactory 工厂类）

---
+++ include/odb.h
@@ -0,0 +1,36 @@
+
+#pragma once
+
+/*
+    odb 封装
+    1. struct mysql_settings  mysql 连接参数
+    2. ODBFactory  odb 工厂类，用于创建 odb handle 句柄
+*/
+
+#include <string>
+#include <odb/database.hxx>
+#include <odb/mysql/transaction.hxx>
+#include <odb/mysql/database.hxx>
+
+namespace cpp_toolkit
+{
+
+struct mysql_settings
+{
+    std::string host;
+    unsigned int port = 0;
+    std::string user;
+    std::string password;
+    std::string database;
+    std::string charset = "utf8";
+    size_t connection_pool_size = 3;
+};
+
+class ODBFactory
+{
+public:
+    static std::shared_ptr<odb::database> Create(const mysql_settings& settings);
+};
+
+
+} // namespace cpp_toolkit
---
+++ src/odb.cpp
@@ -0,0 +1,17 @@
+
+#include "odb.h"
+
+namespace cpp_toolkit
+{
+std::shared_ptr<odb::database> ODBFactory::Create(const mysql_settings& settings)
+{
+    std::unique_ptr<odb::mysql::connection_factory> pool(new odb::mysql::connection_pool_factory(settings.connection_pool_size));
+    auto handle = std::make_shared<odb::mysql::database>(
+        settings.user.c_str(), settings.password.c_str(), settings.database.c_str(), 
+        settings.host.c_str(), settings.port, nullptr ,settings.charset.c_str(), 0 , std::move(pool));
+    return handle;
+}
+
+
+
+} // namespace cpp_toolkit
